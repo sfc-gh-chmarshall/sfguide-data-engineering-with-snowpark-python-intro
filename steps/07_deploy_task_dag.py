5af05e2ef58f882b3d0ecac694a48ffe5becd484
Christen Marshall <christen.marshall@snowflake.com>
2024-06-13 15:43:10 -0500

    Fixed dagtask 1 sp name

--- steps/07_deploy_task_dag.py
+++ steps/07_deploy_task_dag.py
@@ -30,7 +30,7 @@ def main(session: Session) -> str:
     dag_name = "HOL_DAG"
     dag = DAG(dag_name, schedule=timedelta(days=1), warehouse=warehouse_name)
     with dag:
-        dag_task1 = DAGTask("LOAD_ORDER_DETAIL_TASK", definition="CALL LOAD_EXCEL_WORKSHEET_TO_TABLE_SP(BUILD_SCOPED_FILE_URL(@FROSTBYTE_RAW_STAGE, 'intro/order_detail.xlsx'), 'order_detail', 'ORDER_DETAIL')", warehouse=warehouse_name)
+        dag_task1 = DAGTask("LOAD_ORDER_DETAIL_TASK", definition="CALL load_raw_data_sp()", warehouse=warehouse_name)
         dag_task2 = DAGTask("LOAD_DAILY_CITY_METRICS_TASK", definition="CALL LOAD_DAILY_CITY_METRICS_SP()", warehouse=warehouse_name)
 
         dag_task2 >> dag_task1
